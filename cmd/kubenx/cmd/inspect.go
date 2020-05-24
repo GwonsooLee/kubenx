f5028eb24159d70becf315518054917edaba2229
Gwonsoo Lee <gslee@bighitcorp.com>
2020-05-24 16:36:07 +0900

    add help command for first sub command

--- cmd/kubenx/cmd/inspect.go
+++ cmd/kubenx/cmd/inspect.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -28,7 +13,7 @@ func NewCmdInspect() *cobra.Command {
 		AddSearchGroups().
 		SetAliases([]string{"ins"}).
 		AddInspectGroups().
-		RunWithNoArgs(execInsepct)
+		RunWithArgsAndCmd(execInsepct)
 }
 
 // inspectCmd represents the inspect command
@@ -42,6 +27,7 @@ var inspectCmd = &cobra.Command{
 
 
 // Function for search execution
-func execInsepct(_ context.Context, out io.Writer) error {
+func execInsepct(_ context.Context, _ io.Writer, cmd *cobra.Command, args []string) error {
+	cmd.Help()
 	return nil
 }
